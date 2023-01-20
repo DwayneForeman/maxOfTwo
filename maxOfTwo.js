b90a96f2502829ed6fdd5e4d6978e038659554b7
Dwayne Foreman <dwayneforemanjr@yahoo.com>
2023-01-20 00:57:46 -0500

    first commit

---
+++ maxOfTwo.js
@@ -0,0 +1,15 @@
+let x = 1
+let y = 2
+
+// Write your code below this line.
+
+if(x > y){
+  console.log(y);
+} else if (y > x) {
+  console.log(y);
+}
+else {
+  console.log("The values are identical");
+}
+
+
